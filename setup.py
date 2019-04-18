5e3b6c9a3e4f1eb2dd798233c0369a0f6a624dc9
Navallo <zengzhh6@live.com>
2019-04-18 00:38:49 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='collision_avoidance',
       version='0.0.1',
-      packages=['collision_avoidance','collision_avoidance.envs'],
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym'],  # And any other dependencies foo needs
+      packages=['collision_avoidance','collision_avoidance.envs']
 )
