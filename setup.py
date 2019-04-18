d4eaa50e1d0fab027db96d6b8a8e61915cd144c9
Navallo <zengzhh6@live.com>
2019-04-17 23:54:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='collision_avoidance',
       version='0.0.1',
+      packages=['collision_avoidance', 'collision_avoidance.utils','collision_avoidance.envs'],
       install_requires=['gym']  # And any other dependencies foo needs
 )
